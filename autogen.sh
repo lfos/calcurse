921a66ac5afb3515b383d0e1ad6e758cc8ab5ee0
Frederic Culot <calcurse@culot.org>
2009-07-05 20:32:28 +0000

    Switch to BSD license

--- autogen.sh
+++ autogen.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 #
-# Copyright (c) 2004-2006, 2009 Frederic Culot
-#                                                                      
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#                                                                      
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#                                                                      
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software       
-# Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
-#                                                                       
-# Send your feedback or comments to : calcurse@culot.org
-# Calcurse home page : http://culot.org/calcurse
+# Copyright (c) 2004-2006, 2009 Frederic Culot <frederic@culot.org>
+# All rights reserved.
 #
-#        $calcurse: autogen.sh,v 1.2 2009/06/22 16:20:05 culot Exp $
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#      - Redistributions of source code must retain the above
+#        copyright notice, this list of conditions and the
+#        following disclaimer.
+#
+#      - Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the
+#        following disclaimer in the documentation and/or other
+#        materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 #
 # autogen.sh - Generates all the necessary files to build calcurse from
@@ -33,7 +39,7 @@ AUTOCONF_VERSION=2.59
 AC_FLAGS=
 AM_VERSION="1 9"
 AUTOMAKE_VERSION=1.9
-AM_FLAGS="--gnu --copy --add-missing"
+AM_FLAGS="--foreign --copy --add-missing"
 GETTEXT_VERSION="0 14"
 GETTEXT_FLAGS="--copy --no-changelog"
 ACLOCAL_FLAGS="-I m4"
