dfc98b5fa1f7744b17ead661f2919f5b7c2105a7
Lukas Fleischer <calcurse@cryptocrack.de>
2012-01-22 16:55:43 +0100

    test/: Add test case for "-S" (pattern search)
    * search-001.sh: Compare the output of "-S <regex>" (using a regular
      expression) with hardcoded data.
    
    Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

---
+++ test/search-001.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ ! -x "$(command -v faketime)" ]; then
+  echo "libfaketime not found - skipping $0..."
+  exit 1
+fi
+
+if [ "$1" = 'actual' ]; then
+  calcurse -D data/ -s01/01/1902 -r36500 -S '^[KMS]an.*or'
+elif [ "$1" = 'expected' ]; then
+  cat <<EOD
+12/06/42:
+ - 09:46 -> ..:..
+	Manuel glorified four
+
+12/07/42:
+ - ..:.. -> 04:33
+	Manuel glorified four
+
+05/28/85:
+ * Sandbox processor's overdraft's
+EOD
+else
+  ./run-test "$0"
+fi
