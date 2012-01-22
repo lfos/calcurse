cb69a4c0871fc26f85f0133ba4fba0acbc523be4
Lukas Fleischer <calcurse@cryptocrack.de>
2012-01-22 16:39:00 +0100

    test/: Add test cases for "-a" and "-n"
    * appointment-001.sh: Compare output of "-a" with hardcoded data.
    * next-001.sh: Compare output of "-n" with hardcoded data.
    
    Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

---
+++ test/appointment-001.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -x "$(command -v faketime)" ]; then
+  echo "libfaketime not found - skipping $0..."
+  exit 1
+fi
+
+if [ "$1" = 'actual' ]; then
+  faketime '2011-02-25 23:42' calcurse -D data -a
+elif [ "$1" = 'expected' ]; then
+  cat <<EOD
+02/25/11:
+ * Socket ghastlier
+ - ..:.. -> ..:..
+	Covenants useful smoker's
+EOD
+else
+  ./run-test "$0"
+fi
---
+++ test/next-001.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -x "$(command -v faketime)" ]; then
+  echo "libfaketime not found - skipping $0..."
+  exit 1
+fi
+
+if [ "$1" = 'actual' ]; then
+  faketime '1912-07-10 04:10' calcurse -D data -n
+elif [ "$1" = 'expected' ]; then
+  cat <<EOD
+next appointment:
+   [13:37] Impersonating integer broils blame
+EOD
+else
+  ./run-test "$0"
+fi
