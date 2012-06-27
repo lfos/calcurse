b526d46fae3c9c18b532f808687f3afdafbd1ce4
Lukas Fleischer <calcurse@cryptocrack.de>
2012-06-27 12:17:12 +0200

    Add a test case to reproduce BUG#2
    Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

---
+++ test/bug-002.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$1" = 'actual' ]; then
+  "$CALCURSE" --read-only -D "$DATA_DIR"/ -c "$DATA_DIR/apts-bug-002" \
+    -d05/03/2012
+elif [ "$1" = 'expected' ]; then
+  cat <<EOD
+05/03/12:
+ - 10:45 -> 12:45
+	Quantum Mechanics
+ - 18:30 -> 21:30
+	German Class
+EOD
+else
+  ./run-test "$0"
+fi
