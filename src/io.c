lfos/calcurse

commit 691f8a6015bafcf6ed4f99a3649d428fb7a8e915

Refactor startup screen branch
Removes the pointless first parameter to io_startup_screen() and saves
one wins_update() call if system dialogs are disabled.

Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

src/io.c
@@ -988,23 +988,17 @@ io_check_data_files (void)
 
 /* Draw the startup screen */
 void
-io_startup_screen (unsigned show_dialogs, int no_data_file)
+io_startup_screen (int no_data_file)
 {
-  const char *welcome_mesg =
-    _("Welcome to Calcurse. Missing data files were created.");
-  const char *data_mesg = _("Data files found. Data will be loaded now.");
   const char *enter = _("Press [ENTER] to continue");
 
-  if (no_data_file != 0)
-    {
-      status_mesg (welcome_mesg, enter);
-      wgetch (win[STA].p);
-    }
-  else if (show_dialogs)
-    {
-      status_mesg (data_mesg, enter);
-      wgetch (win[STA].p);
-    }
+  if (no_data_file)
+    status_mesg (_("Data files found. Data will be loaded now."), enter);
+  else
+    status_mesg (_("Welcome to Calcurse. Missing data files were created."),
+                 enter);
+
+  wgetch (win[STA].p);
 }
 
 /* Export calcurse data. */

src/calcurse.c
@@ -161,8 +161,11 @@ main (int argc, char **argv)
   wins_reinit ();
   if (notify_bar ())
       notify_start_main_thread ();
-  wins_update (FLAG_ALL);
-  io_startup_screen (conf.system_dialogs, no_data_file);
+  if (conf.system_dialogs)
+    {
+      wins_update (FLAG_ALL);
+      io_startup_screen (no_data_file);
+    }
   inday = *day_process_storage (0, 0, &inday);
   wins_slctd_set (CAL);
   wins_update (FLAG_ALL);

src/calcurse.h
@@ -703,7 +703,7 @@ void             io_check_dir (char *, int *);
 unsigned         io_file_exist (char *);
 void             io_check_file (char *, int *);
 int              io_check_data_files (void);
-void             io_startup_screen (unsigned, int);
+void             io_startup_screen (int);
 void             io_export_data (enum export_type);
 void             io_export_bar (void);
 void             io_import_data (enum import_type, char *);

commit 82942938be77f407d9bb67b81160e0160e9130e3

Display correct startup messages
These messages have been swapped accidentally during refactoring.
Regression introduced in 691f8a6015bafcf6ed4f99a3649d428fb7a8e915.

Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

src/io.c
@@ -888,10 +888,10 @@ void io_startup_screen(int no_data_file)
   const char *enter = _("Press [ENTER] to continue");
 
   if (no_data_file)
-    status_mesg(_("Data files found. Data will be loaded now."), enter);
-  else
     status_mesg(_("Welcome to Calcurse. Missing data files were created."),
                 enter);
+  else
+    status_mesg(_("Data files found. Data will be loaded now."), enter);
 
   wgetch(win[KEY].p);
 }