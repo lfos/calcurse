lfos/calcurse

commit a363cb9b9111aed22d105adb0e7a8e9caab9bbe3

Fix braces in if-else statements
From the Linux kernel coding guidelines:

    Do not unnecessarily use braces where a single statement will do.
    [...] This does not apply if one branch of a conditional statement
    is a single statement. Use braces in both branches.

Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

src/utils.c
@@ -77,8 +77,9 @@ void exit_calcurse(int status)
     endwin();
     ui_mode = UI_CMDLINE;
     was_interactive = 1;
-  } else
+  } else {
     was_interactive = 0;
+  }
 
   ui_calendar_stop_date_thread();
   io_stop_psave_thread();
@@ -379,9 +380,9 @@ char *date_sec2date_str(long sec, const char *datefmt)
   struct tm lt;
   char *datestr = (char *)mem_calloc(BUFSIZ, sizeof(char));
 
-  if (sec == 0)
-    strncpy(datestr, "0", BUFSIZ);
-  else {
+  if (sec == 0) {
+    strncpy(datestr, "0", BUFSIZ)
+  } else {
     localtime_r((time_t *)&sec, &lt);
     strftime(datestr, BUFSIZ, datefmt, &lt);
   }
@@ -574,8 +575,9 @@ print_bool_option_incolor(WINDOW * win, unsigned option, int pos_y, int pos_x)
   } else if (option == 0) {
     color = ATTR_FALSE;
     option_value = _("no");
-  } else
+  } else {
     EXIT(_("option not defined"));
+  }
 
   custom_apply_attr(win, color);
   mvwaddstr(win, pos_y, pos_x, option_value);
@@ -660,10 +662,11 @@ parse_date(const char *date_string, enum datefmt datefmt, int *year,
     if (*p == sep) {
       if ((++n) > 2)
         return 0;
-    } else if ((*p >= '0') && (*p <= '9'))
+    } else if ((*p >= '0') && (*p <= '9')) {
       in[n] = in[n] * 10 + (int)(*p - '0');
-    else
+    } else {
       return 0;
+    }
   }
 
   if ((!slctd_date && n < 2) || in[n] == 0)
@@ -741,8 +744,9 @@ int parse_time(const char *string, unsigned *hour, unsigned *minute)
       if ((n == 0) && (p == (string + 2)) && *(p + 1))
         n++;
       in[n] = in[n] * 10 + (int)(*p - '0');
-    } else
+    } else {
       return 0;
+    }
   }
 
   if (n != 1 || in[0] >= DAYINHOURS || in[1] >= HOURINMIN)
@@ -804,8 +808,9 @@ int parse_duration(const char *string, unsigned *duration)
         } else if (*p == 'm') {
           dur += in;
           state = STATE_DONE;
-        } else
+        } else {
           return 0;
+        }
         break;
       case STATE_DDHHMM_HH:
         if (*p == 'h') {
@@ -814,15 +819,17 @@ int parse_duration(const char *string, unsigned *duration)
         } else if (*p == 'm') {
           dur += in;
           state = STATE_DONE;
-        } else
+        } else {
           return 0;
+        }
         break;
       case STATE_DDHHMM_MM:
         if (*p == 'm') {
           dur += in;
           state = STATE_DONE;
-        } else
+        } else {
           return 0;
+        }
         break;
       case STATE_HHMM_MM:
         return 0;
@@ -1015,8 +1022,9 @@ static void print_notefile(FILE * out, const char *filename, int nbtab)
     for (i = 0; i < nbtab; i++)
       linestarter[i] = '\t';
     linestarter[nbtab] = '\0';
-  } else
+  } else {
     linestarter[0] = '\0';
+  }
 
   snprintf(path_to_notefile, BUFSIZ, "%s/%s", path_notes, filename);
   notefile = fopen(path_to_notefile, "r");
@@ -1168,9 +1176,9 @@ static void print_date(long date, long day, const char *extformat)
 {
   char buf[BUFSIZ];
 
-  if (!strcmp(extformat, "epoch"))
+  if (!strcmp(extformat, "epoch")) {
     printf("%ld", date);
-  else {
+  } else {
     time_t t = date;
     struct tm lt;
 
@@ -1227,10 +1235,11 @@ void print_apoint(const char *format, long day, struct apoint *apt)
         putchar('?');
         break;
       }
-    } else if (*p == '\\')
+    } else if (*p == '\\') {
       p += print_escape(p);
-    else
+    } else {
       putchar(*p);
+    }
   }
 }
 
@@ -1263,10 +1272,11 @@ void print_event(const char *format, long day, struct event *ev)
         putchar('?');
         break;
       }
-    } else if (*p == '\\')
+    } else if (*p == '\\') {
       p += print_escape(p);
-    else
+    } else {
       putchar(*p);
+    }
   }
 }
 
@@ -1328,9 +1338,10 @@ void print_todo(const char *format, struct todo *todo)
         putchar('?');
         break;
       }
-    } else if (*p == '\\')
+    } else if (*p == '\\') {
       p += print_escape(p);
-    else
+    } else {
       putchar(*p);
+    }
   }
 }

src/apoint.c
@@ -117,16 +117,16 @@ void apoint_sec2str(struct apoint *o, long day, char *start, char *end)
   struct tm lt;
   time_t t;
 
-  if (o->start < day)
+  if (o->start < day) {
     strncpy(start, "..:..", 6);
-  else {
+  } else {
     t = o->start;
     localtime_r(&t, &lt);
     snprintf(start, HRMIN_SIZE, "%02u:%02u", lt.tm_hour, lt.tm_min);
   }
-  if (o->start + o->dur > day + DAYINSEC)
+  if (o->start + o->dur > day + DAYINSEC) {
     strncpy(end, "..:..", 6);
-  else {
+  } else {
     t = o->start + o->dur;
     localtime_r(&t, &lt);
     snprintf(end, HRMIN_SIZE, "%02u:%02u", lt.tm_hour, lt.tm_min);

src/args.c
@@ -561,8 +561,9 @@ int parse_args(int argc, char **argv)
           usage_try();
           return EXIT_FAILURE;
         }
-      } else
+      } else {
         tnum = -1;
+      }
       break;
     case 'v':
       vflag = 1;
@@ -572,11 +573,11 @@ int parse_args(int argc, char **argv)
       multiple_flag++;
       load_data++;
       if (optarg != NULL) {
-        if (strcmp(optarg, "ical") == 0)
+        if (strcmp(optarg, "ical") == 0) {
           xfmt = IO_EXPORT_ICAL;
-        else if (strcmp(optarg, "pcal") == 0)
+        } else if (strcmp(optarg, "pcal") == 0) {
           xfmt = IO_EXPORT_PCAL;
-        else {
+        } else {
           fputs(_("Argument for '-x' should be either "
                   "'ical' or 'pcal'\n"), stderr);
           usage();

src/calcurse.c
@@ -256,8 +256,9 @@ static inline void key_edit_note(void)
   if (wins_slctd() == APP && ui_day_hilt() != 0) {
     day_edit_note(day_get_item(ui_day_hilt()), conf.editor);
     inday = do_storage(0);
-  } else if (wins_slctd() == TOD && ui_todo_hilt() != 0)
+  } else if (wins_slctd() == TOD && ui_todo_hilt() != 0) {
     todo_edit_note(todo_get_item(ui_todo_hilt()), conf.editor);
+  }
   wins_update(FLAG_ALL);
 }
 
@@ -465,14 +466,15 @@ static inline void key_generic_quit(void)
     note_gc();
 
   if (conf.confirm_quit) {
-    if (status_ask_bool(_("Do you really want to quit ?")) == 1)
+    if (status_ask_bool(_("Do you really want to quit ?")) == 1) {
       exit_calcurse(EXIT_SUCCESS);
-    else {
+    } else {
       wins_erase_status_bar();
       wins_update(FLAG_STA);
     }
-  } else
+  } else {
     exit_calcurse(EXIT_SUCCESS);
+  }
 }
 
 /*

src/config.c
@@ -275,8 +275,9 @@ static int config_parse_input_datefmt(void *dummy, const char *val)
     if (conf.input_datefmt <= 0 || conf.input_datefmt > DATE_FORMATS)
       conf.input_datefmt = 1;
     return 1;
-  } else
+  } else {
     return 0;
+  }
 }
 
 /* Set a configuration variable. */
@@ -359,16 +360,16 @@ static void config_color_theme_name(char *theme_name)
     "white"
   };
 
-  if (!colorize)
+  if (!colorize) {
     strncpy(theme_name, "none", BUFSIZ);
-  else {
+  } else {
     pair_content(COLR_CUSTOM, &color[0], &color[1]);
     for (i = 0; i < NBCOLORS; i++) {
-      if ((color[i] == DEFAULTCOLOR) || (color[i] == DEFAULTCOLOR_EXT))
+      if ((color[i] == DEFAULTCOLOR) || (color[i] == DEFAULTCOLOR_EXT)) {
         color_name[i] = default_color;
-      else if (color[i] >= 0 && color[i] <= MAXCOLORS)
+      } else if (color[i] >= 0 && color[i] <= MAXCOLORS) {
         color_name[i] = name[color[i]];
-      else {
+      } else {
         EXIT(_("unknown color"));
         /* NOTREACHED */
       }
@@ -453,8 +454,9 @@ config_serialize_conf(char *buf, const char *key,
         if (status)
           status->done[i] = 1;
         return 1;
-      } else
+      } else {
         return 0;
+      }
     }
   }
 
@@ -489,8 +491,7 @@ config_file_walk(config_fn_walk_cb_t fn_cb,
     if (value) {
       *value = '\0';
       value++;
-    }
-    else {
+    } else {
       EXIT(_("invalid configuration directive: \"%s\""), e_conf);
     }
 
@@ -538,12 +539,13 @@ static int config_load_cb(const char *key, const char *value, void *dummy)
 {
   int result = config_set_conf(key, value);
 
-  if (result < 0)
+  if (result < 0) {
     EXIT(_("configuration variable unknown: \"%s\""), key);
-  /* NOTREACHED */
-  else if (result == 0)
+    /* NOTREACHED */
+  } else if (result == 0) {
     EXIT(_("wrong configuration variable format for \"%s\""), key);
-  /* NOTREACHED */
+    /* NOTREACHED */
+  }
 
   return 1;
 }
@@ -560,12 +562,13 @@ static int config_save_cb(const char *key, const char *value, void *status)
   int result =
       config_serialize_conf(buf, key, (struct config_save_status *)status);
 
-  if (result < 0)
+  if (result < 0) {
     EXIT(_("configuration variable unknown: \"%s\""), key);
-  /* NOTREACHED */
-  else if (result == 0)
+    /* NOTREACHED */
+  } else if (result == 0) {
     EXIT(_("wrong configuration variable format for \"%s\""), key);
-  /* NOTREACHED */
+    /* NOTREACHED */
+  }
 
   fputs(key, ((struct config_save_status *)status)->fp);
   fputc('=', ((struct config_save_status *)status)->fp);

src/custom.c
@@ -455,19 +455,21 @@ display_color_config(struct window *cwin, int *mark_fore, int *mark_back,
       /* Retrieve the actual color theme. */
       pair_content(COLR_CUSTOM, &colr_fore, &colr_back);
 
-      if ((colr_fore == DEFAULTCOLOR) || (colr_fore == DEFAULTCOLOR_EXT))
+      if ((colr_fore == DEFAULTCOLOR) || (colr_fore == DEFAULTCOLOR_EXT)) {
         *mark_fore = NBUSERCOLORS;
-      else
+      } else {
         for (i = 0; i < NBUSERCOLORS + 1; i++)
           if (colr_fore == colr[i])
             *mark_fore = i;
+      }
 
-      if ((colr_back == DEFAULTCOLOR) || (colr_back == DEFAULTCOLOR_EXT))
+      if ((colr_back == DEFAULTCOLOR) || (colr_back == DEFAULTCOLOR_EXT)) {
         *mark_back = SIZE - 1;
-      else
+      } else {
         for (i = 0; i < NBUSERCOLORS + 1; i++)
           if (colr_back == colr[NBUSERCOLORS + 1 + i])
             *mark_back = NBUSERCOLORS + 1 + i;
+      }
     }
   }
 
@@ -976,8 +978,9 @@ void custom_keys_config(void)
                                          LINESPERKEY);
           wins_scrollwin_display(&kwin);
           continue;
-        } else
+        } else {
           not_recognized = 0;
+        }
 
         /* Is the binding used by this action already? If so, just end the reassignment */
         if (selrow == keys_get_action(keyval)) {
@@ -1041,9 +1044,9 @@ void custom_config_main(void)
     switch (ch) {
     case 'C':
     case 'c':
-      if (has_colors())
+      if (has_colors()) {
         custom_color_config();
-      else {
+      } else {
         colorize = 0;
         wins_erase_status_bar();
         mvwaddstr(win[STA].p, 0, 0, no_color_support);

src/day.c
@@ -72,10 +72,11 @@ static int day_cmp_start(struct day_item *a, struct day_item *b)
       return 0;
     else
       return -1;
-  } else if (b->type <= EVNT)
+  } else if (b->type <= EVNT) {
     return 1;
-  else
+  } else {
     return a->start < b->start ? -1 : (a->start == b->start ? 0 : 1);
+  }
 }
 
 /* Add an item to the current day list. */
@@ -446,9 +447,9 @@ display_item(struct day_item *day, int incolor, int width, int y, int x)
   ch_note = day_item_get_note(day) ? '>' : ' ';
   if (incolor == 0)
     custom_apply_attr(win, ATTR_HIGHEST);
-  if (utf8_strwidth(mesg) < width)
+  if (utf8_strwidth(mesg) < width) {
     mvwprintw(win, y, x, " %c%c%s", ch_recur, ch_note, mesg);
-  else {
+  } else {
     for (i = 0; mesg[i] && width > 0; i++) {
       if (!UTF8_ISCONT(mesg[i]))
         width -= utf8_width(&mesg[i]);

src/getstring.c
@@ -193,8 +193,9 @@ enum getstr getstring(WINDOW * win, char *str, int l, int x, int y)
       if (st.pos > 0) {
         st.pos--;
         getstr_del_char(&st);
-      } else
+      } else {
         bell();
+      }
       break;
     case CTRL('D'):            /* delete next character */
       if (st.pos < st.len)
@@ -212,8 +213,9 @@ enum getstr getstring(WINDOW * win, char *str, int l, int x, int y)
           st.pos--;
           getstr_del_char(&st);
         }
-      } else
+      } else {
         bell();
+      }
       break;
     case CTRL('K'):            /* delete to end-of-line */
       st.s[st.ci[st.pos].offset] = 0;

commit 3d7fdaec584ad7080d187b8aa53c6546d9c1d8d7

date_sec2date_str(): Add missing semicolon
Regression introduced in a363cb9b9111aed22d105adb0e7a8e9caab9bbe3.

Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

src/utils.c
@@ -381,7 +381,7 @@ char *date_sec2date_str(long sec, const char *datefmt)
   char *datestr = (char *)mem_calloc(BUFSIZ, sizeof(char));
 
   if (sec == 0) {
-    strncpy(datestr, "0", BUFSIZ)
+    strncpy(datestr, "0", BUFSIZ);
   } else {
     localtime_r((time_t *)&sec, &lt);
     strftime(datestr, BUFSIZ, datefmt, &lt);