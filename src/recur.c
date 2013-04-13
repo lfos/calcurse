lfos/calcurse

commit 9907069f442c56c90b67accb2d8fbd046dfce6db

Validate date/time when scanning items
Bail out when reading dates such as "02/30/2013" from the appointments
file. These *could* be converted into valid dates but since we never
write invalid dates to that file, these indicate a user error.

Fixes following test cases:

* appointment-009.sh
* appointment-012.sh
* appointment-016.sh
* appointment-019.sh
* event-003.sh

Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

src/recur.c
@@ -325,6 +325,14 @@ struct recur_apoint *recur_apoint_scan(FILE * f, struct tm start, struct tm end,
   char buf[BUFSIZ], *nl;
   time_t tstart, tend, tuntil;
 
+  EXIT_IF(!check_date(start.tm_year, start.tm_mon, start.tm_mday) ||
+          !check_date(end.tm_year, end.tm_mon, end.tm_mday) ||
+          !check_time(start.tm_hour, start.tm_min) ||
+          !check_time(end.tm_hour, end.tm_min) ||
+          (until.tm_year != 0 && !check_date(until.tm_year, until.tm_mon,
+                                             until.tm_mday)),
+          _("date error in appointment"));
+
   /* Read the appointment description */
   if (!fgets(buf, sizeof buf, f))
     return NULL;
@@ -367,6 +375,12 @@ struct recur_event *recur_event_scan(FILE * f, struct tm start, int id,
 {
   char buf[BUFSIZ], *nl;
   time_t tstart, tuntil;
+
+  EXIT_IF(!check_date(start.tm_year, start.tm_mon, start.tm_mday) ||
+          !check_time(start.tm_hour, start.tm_min) ||
+          (until.tm_year != 0 && !check_date(until.tm_year, until.tm_mon,
+                                             until.tm_mday)),
+          _("date error in event"));
 
   /* Read the event description */
   if (!fgets(buf, sizeof buf, f))
@@ -750,6 +764,11 @@ void recur_exc_scan(llist_t * lexc, FILE * data_file)
                &day.tm_mon, &day.tm_mday, &day.tm_year) != 3) {
       EXIT(_("syntax error in item date"));
     }
+
+    EXIT_IF(!check_date(day.tm_year, day.tm_mon, day.tm_mday) ||
+            !check_time(day.tm_hour, day.tm_min),
+            _("date error in item exception"));
+
     day.tm_hour = 0;
     day.tm_min = day.tm_sec = 0;
     day.tm_isdst = -1;

src/apoint.c
@@ -165,6 +165,12 @@ struct apoint *apoint_scan(FILE * f, struct tm start, struct tm end, char state,
   char buf[BUFSIZ], *newline;
   time_t tstart, tend;
 
+  EXIT_IF(!check_date(start.tm_year, start.tm_mon, start.tm_mday) ||
+          !check_date(end.tm_year, end.tm_mon, end.tm_mday) ||
+          !check_time(start.tm_hour, start.tm_min) ||
+          !check_time(end.tm_hour, end.tm_min),
+          _("date error in appointment"));
+
   /* Read the appointment description */
   if (!fgets(buf, sizeof buf, f))
     return NULL;

src/event.c
@@ -125,6 +125,10 @@ struct event *event_scan(FILE * f, struct tm start, int id, char *note)
   char buf[BUFSIZ], *nl;
   time_t tstart;
 
+  EXIT_IF(!check_date(start.tm_year, start.tm_mon, start.tm_mday) ||
+          !check_time(start.tm_hour, start.tm_min),
+          _("date error in event"));
+
   /* Read the event description */
   if (!fgets(buf, sizeof buf, f))
     return NULL;

commit ec837150a0910026c60bf51ccf58db2a0e2fb5e0

recur_exc_scan(): Do not check time of exceptions
Exceptions do not contain a time field -- do not check time fields which
may be uninitialized.

Regression introduced in 9907069f442c56c90b67accb2d8fbd046dfce6db. This
fixes test/recur-*.sh.

Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

src/recur.c
@@ -783,8 +783,7 @@ void recur_exc_scan(llist_t * lexc, FILE * data_file)
 			EXIT(_("syntax error in item date"));
 		}
 
-		EXIT_IF(!check_date(day.tm_year, day.tm_mon, day.tm_mday)
-			|| !check_time(day.tm_hour, day.tm_min),
+		EXIT_IF(!check_date(day.tm_year, day.tm_mon, day.tm_mday),
 			_("date error in item exception"));
 
 		day.tm_hour = 0;