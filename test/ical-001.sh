e57b9654ff90230bc8b0631753b3dc0c829a9747
Lukas Fleischer <calcurse@cryptocrack.de>
2014-08-18 11:32:50 +0200

    test/: Fix date in ical-001
    Signed-off-by: Lukas Fleischer <calcurse@cryptocrack.de>

--- test/ical-001.sh
+++ test/ical-001.sh
@@ -6,18 +6,18 @@ if [ "$1" = 'actual' ]; then
   mkdir .calcurse || exit 1
   cp "$DATA_DIR/conf" .calcurse || exit 1
   "$CALCURSE" -D "$PWD/.calcurse" -i "$DATA_DIR/ical-001.ical"
-  "$CALCURSE" -D "$PWD/.calcurse" -s01/01/1902 -r2
+  "$CALCURSE" -D "$PWD/.calcurse" -s01/01/1980 -r2
   "$CALCURSE" -D "$PWD/.calcurse" -t
   rm -rf .calcurse || exit 1
 elif [ "$1" = 'expected' ]; then
   cat <<EOD
 Import process report: 0017 lines read
 1 app / 0 events / 1 todo / 0 skipped
-01/01/02:
+01/01/80:
  - 00:01 -> ..:..
 	Calibrator's
 
-01/02/02:
+01/02/80:
  - ..:.. -> 09:18
 	Calibrator's
 to do:
