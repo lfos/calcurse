0b7614a4a7a88167066ee28553d278ac4bb66714
Lukas Fleischer <lfleischer@calcurse.org>
2016-01-10 22:53:04 +0100

    Reset status page when opening configuration
    Signed-off-by: Lukas Fleischer <lfleischer@calcurse.org>

--- src/calcurse.c
+++ src/calcurse.c
@@ -98,6 +98,7 @@ static inline void key_view_item(void)
 static inline void key_generic_config_menu(void)
 {
 	wins_erase_status_bar();
+	wins_reset_status_page();
 	custom_config_main();
 	do_storage(0);
 	wins_update(FLAG_ALL);
