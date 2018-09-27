lfos/calcurse

commit 6a6c7117dee91c9fd11c2b30f34f70386f1fba21

calcurse-caldav: fix basic authentication
Read whether basic authentication is used and add the credentials to
httplib2 when making a request.

Signed-off-by: Lukas Fleischer <lfleischer@calcurse.org>

contrib/caldav/calcurse-caldav.py
@@ -656,6 +656,11 @@ try:
         # Authenticate with OAuth2 and authorize HTTP object
         cred = run_auth(authcode)
         conn = cred.authorize(conn)
+
+    # Add credentials to httplib2 to be used when request requires authentication
+    if authmethod == 'basic':
+        conn.add_credentials(username, password)
+
     elif authmethod != 'basic':
         die('Invalid option for AuthMethod in config file. Use "basic" or "oauth2"')
 

commit c4960aaf9346d99722f08eb6c91e9fa34e0adbc7

calcurse-caldav: fix auth method check
Fixes a regression introduced in 6a6c711 (calcurse-caldav: fix basic
authentication, 2018-07-20).

Addresses GitHub issue #149.

Signed-off-by: Lukas Fleischer <lfleischer@calcurse.org>

contrib/caldav/calcurse-caldav.py
@@ -688,12 +688,10 @@ try:
         # Authenticate with OAuth2 and authorize HTTP object
         cred = run_auth(authcode)
         conn = cred.authorize(conn)
-
-    # Add credentials to httplib2 to be used when request requires authentication
-    if authmethod == 'basic':
+    elif authmethod == 'basic':
+        # Add credentials to httplib2
         conn.add_credentials(username, password)
-
-    elif authmethod != 'basic':
+    else:
         die('Invalid option for AuthMethod in config file. Use "basic" or "oauth2"')
 
     if init: