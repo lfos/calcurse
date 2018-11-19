a5e660899217e8ca552096375db6be0f83f939ea
Lars Henriksen <LarsHenriksen@get2net.dk>
2018-11-19 18:56:48 +0100

    CLI: tests aligned
    Signed-off-by: Lukas Fleischer <lfleischer@calcurse.org>

--- test/regress-001.sh
+++ test/regress-001.sh
@@ -4,7 +4,7 @@
 
 if [ "$1" = 'actual' ]; then
   "$CALCURSE" --read-only -D "$DATA_DIR"/ -c "$DATA_DIR/apts-regress-001" \
-    -Q --filter-type=cal --from=2016-03-27 --days=2
+    -Q --filter-type=cal --from=03/27/2016 --days=2
 elif [ "$1" = 'expected' ]; then
   cat <<EOD
 03/28/16:
