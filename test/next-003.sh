3710e385be4c3fea87263284af897d03690c100a
Lars Henriksen <LarsHenriksen@get2net.dk>
2019-04-11 20:02:54 +0200

    New test cases for next appoinment
    The change by commit "Fix display of time left before next appointment" is
    not detected by the existing test because the time set has zero seconds.

---
+++ test/next-003.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+. "${TEST_INIT:-./test-init.sh}"
+
+if [ ! -x "$(command -v faketime)" ]; then
+  echo "libfaketime not found - skipping $0..."
+  exit 0
+fi
+
+if [ "$1" = 'actual' ]; then
+  faketime -f '1912-07-10 04:10:01' "$CALCURSE" --read-only -D "$DATA_DIR" -n
+elif [ "$1" = 'expected' ]; then
+  cat <<EOD
+next appointment:
+   [13:37] Impersonating integer broils blame
+EOD
+else
+  ./run-test "$0"
+fi
